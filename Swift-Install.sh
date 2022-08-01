a50759747e9416071d74398065401e086cae136e
roto31 <47955141+roto31@users.noreply.github.com>
2022-07-31 20:31:18 -0400

    Update Swift-Install.sh
    Add script with explanations of each path and function

--- Swift-Install.sh
+++ Swift-Install.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
+#Swift-Installer
+#Based on swiftDialog and Installomater 
+#roto31
+#Credits to Bart Reardon for developing andf maintaining #swiftDialog and the developers and maintainers of the Installomater script
 
 #Variables
-#Applications - Add any Application here with its Installomater label as the variable and Common Name after
+#Applications - Add any Application here with its Installomater label as the variable and Common Name after. Add the common name to the "selectvalues" under 
+# swiftDialog variables. 
 firefoxpkg="Firefox"
 atom="Atom"
 brave="Brave"
 dialogCMD="/usr/local/bin/dialog"
 
+#Default path to Installomater
 path="/usr/local/Installomator/Installomator.sh"
+
+#swiftDialog variables
 title="Select Your Application"
 message="The below applications are available to install for your computer."
 selecttitle="Applications Available"
 selectvalues="Firefox,Atom,Brave"
 
 complete=""
+
 
 cmd="$dialogCMD -p --title '${title}' \
     --message '${message}' \
@@ -33,7 +42,9 @@ while [[ -z $complete ]]; do
     fi
 done
 
+#Install Function
 echo "$result"
 echo "application = $application"
 
 sudo -s $"$path" "$application"
+
